6a65594a0ab60f3ef1bac64127c29e84a4f684a2
Hop2_Bilegt <Hop2_Bilegt@nest-36s-iMac.local>
2023-03-29 18:54:12 +0800

    cart

--- frontend/src/pages/Merch.js
+++ frontend/src/pages/Merch.js
@@ -1,12 +1,38 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import MerchCart from "../components/MerchCart"
+import MerchCart from "../components/MerchCart";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function Merch() {
+  const [data, setData] = useState();
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  const instance = Axios.create({
+    baseURL: "https://dummyapi.io/data/v1/post/",
+    headers: {
+      "app-id": "636e0d6642c1f665f684f489",
+    },
+  });
+
+  useEffect(() => {
+    const fetchProps = async () => {
+      try {
+        const res = await instance.get("/");
+        setData([res.data.data][0].slice(0, 6));
+        console.log(slicedData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProps();
+  }, []);
+
   return (
     <Box sx={{ marginTop: "70px" }}>
       <Container maxWidth="xl">
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+        <Box>
           <Box
             sx={{
               display: "flex",
@@ -17,32 +43,85 @@ export default function Merch() {
             }}
           >
             <Box sx={{ display: "flex", alignSelf: "self-start" }}>
-              <Box sx={{ fontSize: "96px", fontWeight: "bold", }}>MERCH</Box>
-              <Box sx={{ color: "rgb(205, 30, 59)", fontWeight: "bold", fontSize: "36px" }}>
+              <Box sx={{ fontSize: "96px", fontWeight: "bold" }}>MERCH</Box>
+              <Box
+                sx={{
+                  color: "rgb(205, 30, 59)",
+                  fontWeight: "bold",
+                  fontSize: "36px",
+                }}
+              >
                 751
               </Box>
             </Box>
-            <Box sx={{ alignSelf: "self-start", fontWeight:"300" }}>
+            <Box sx={{ alignSelf: "self-start", fontWeight: "300" }}>
               Илүү мэдрэмжийг. Илүү хялбараар
             </Box>
-            <Box sx={{ paddingTop: "16px", paddingBottom: "16px", width:"100%", boxSizing:"border-box" }}>
+            <Box
+              sx={{
+                paddingTop: "16px",
+                paddingBottom: "16px",
+                width: "100%",
+                boxSizing: "border-box",
+              }}
+            >
               <Box
                 sx={{
                   width: "100%",
                   height: "1px",
                   bgcolor: "black",
-                  boxSizing:"border-box"
+                  boxSizing: "border-box",
                 }}
               ></Box>
             </Box>
           </Box>
-          <Box sx={{display:"flex"}}>
-            <Box sx={{flexWrap:"wrap", display:"flex", flexDirection:"row", width:"80%", border:"1px solid red"}}>
-                <MerchCart/>
+          <Box sx={{ display: "flex" }}>
+            <Box
+              sx={{
+                flexWrap: "wrap",
+                display: "flex",
+                flexDirection: "row",
+                width: "75%",
+                gap: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {data?.map((card, index) => (
+                <MerchCart key={index} image={card.image}></MerchCart>
+              ))}
             </Box>
-            <Box sx={{paddingLeft:"40px", paddingRight:"40px", width:"20%", border:"1px solid blue"}}>
-                <Box sx={{padding:"8px"}}>Төрлөөр шүүх</Box>
-                <Box></Box>
+            <Box
+              sx={{
+                paddingLeft: "40px",
+                paddingRight: "40px",
+                width: "25%",
+                height: "894.167px",
+                display: "flex",
+                flexDirection:"column",
+              }}
+            >
+              <Box
+                sx={{
+                  top: "20px",
+                  position: "sticky",
+                  padding: "10px",
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                }}
+              >
+                Төрлөөр шүүх
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Checkbox {...label} />
+                <Box sx={{ display: "flex", alignItems: "center" }}>Бүгд</Box>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Checkbox {...label} />
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  пудволк
+                </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
