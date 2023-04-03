e17a6e11482a3709c598d6b5562435c5371d913e
spaceishere <batocirdaramjav@gmail.com>
2023-04-03 19:18:06 +0800

    is done full resposive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,65 @@
+import Container from "@mui/material/Container";
+
 export default function Footer() {
-  return <div>is Footer</div>;
+  return (
+    <div className="bg-[#1b1927] mb-[60px] pb-[100px] md:pb-[60px] ">
+      <Container>
+        <div className="flex flex-col ">
+          <div className="w-full grid grid-cols-12 h-[500px] pt-[50px] md:pt-[150px] gap-y-[50px]">
+            <div className="col-span-12 sm:col-span-6 xl:col-span-3 w-[200px] flex flex-col gap-[8px]">
+              <div className="mb-[30px] flex flex-col gap-[10px]">
+                <div className="text-white font-bold">INFORMATION</div>
+                <div className="h-[1px] rounded-[2px] bg-red-500"></div>
+              </div>
+              <div className="text-white text-[16px] font-bold flex flex-col items-start gap-[10px]">
+                <p>privacy policy</p>
+                <p>Geru for ...</p>
+                <p>terms of use</p>
+              </div>
+            </div>
+            <div className="col-span-12 sm:col-span-6 xl:col-span-3 w-[220px] flex flex-col gap-[8px]">
+              <div className="mb-[30px] flex flex-col gap-[10px]">
+                <div className="text-white font-bold">ХОЛБОО БАРИХ</div>
+                <div className="h-[1px] rounded-[2px] bg-red-500"></div>
+              </div>
+              <div className="text-white text-[16px] font-bold flex flex-col items-start gap-[10px]">
+                <p>Сүхбаатар дүүрэг, 5-р хороо, New Mind Building, 1 давхар</p>
+                <p>Мэйл: info@geru.mn</p>
+              </div>
+            </div>
+            <div className="col-span-12 sm:col-span-6 xl:col-span-3 w-[220px] flex flex-col gap-[8px]">
+              <div className="mb-[30px] flex flex-col gap-[10px]">
+                <div className="text-white font-bold">FOR LATEST UPDATES</div>
+                <div className="h-[1px] rounded-[2px] bg-red-500"></div>
+              </div>
+              <div className="text-white text-[16px] font-bold flex flex-col items-start gap-[10px] ">
+                <button className="bg-[#464ee3] rounded-[7px] w-[200px] h-[50px]">
+                  JOIN OUR COMMUNITY
+                </button>
+                <button className="h-[50px]">JOIN US ON INSTAGRAM</button>
+                <button className="bg-[#3875e9] rounded-[7px] w-[200px] h-[50px]">
+                  LIKE US ON FACEBOOK
+                </button>
+              </div>
+            </div>
+            <div className="col-span-12 sm:col-span-6 xl:col-span-3 w-[220px] flex flex-col gap-[8px]">
+              <div className="mb-[30px] flex flex-col gap-[10px]">
+                <div className="text-white font-bold">GERU АРТИСТ</div>
+                <div className="h-[1px] rounded-[2px] bg-red-500"></div>
+              </div>
+              <p className="text-white font-bold">
+                Мэдрэмжээ чөлөөтэй илэрхийлэх бүтээлч орон зай Geru-г санал
+                болгож байна
+              </p>
+            </div>
+          </div>
+          <div className="text-white text-[30px] font-bold mt-[500px] sm:mt-[50px] md:mt-[250px] md:text-[52px] lg:mt-[200px] xl:mt-[30px] lg:text-[72px]">
+            ENJOY YOUR
+            <span className="text-red-500">UNIQUENESS</span>
+            🤟
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,11 +1,75 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../context/context";
+import { AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
+import { SlBasket, SlArrowDown } from "react-icons/sl";
+import Container from "@mui/material/Container";
+import Search from "../components/Search";
+
 export default function Header() {
   const { info } = useContext(Context);
+  const [searchClick, setSearchClick] = useState(false);
+  const [burger, setBurger] = useState(false);
+
+  useEffect(() => {
+    const resizeFunc = () => {
+      if (window.innerWidth < 1536) {
+        setSearchClick(false);
+      }
+    };
+    resizeFunc();
+    window.addEventListener("resize", resizeFunc);
+    return () => window.removeEventListener("resize", resizeFunc);
+  }, []);
+
   return (
-    <div>
-      is header
-      <p>{info}</p>
+    <div className="flex flex-col">
+      <Container className="flex justify-between h-[75px] items-center">
+        <div className="flex items-center gap-5 md:gap-5 w-[100%]">
+          <div className="text-[30px] font-bold">Geru</div>
+          <input
+            className="outline-none h-[40px] w-[60%] md:w-[100%] x:hidden font-bold"
+            placeholder="Мэрч, Артист, Коллекц хайх ..."
+          />
+        </div>
+        <div className="flex items-center gap-5">
+          <div className="">
+            <span
+              className="text-[20px] flex items-center gap-2 font-bold"
+              onClick={() => {
+                setBurger(!burger);
+              }}>
+              Enjoy <SlArrowDown className="text-[15px]" />
+            </span>
+            <div
+              className={`${
+                burger ? " flex" : "hidden"
+              } absolute bg-white w-[150px] h-[200px] absolute xl:mt-[-45px] xl:ml-[100px] -mt-[35px] -ml-[160px] xl:-mt-[35px] flex-col px-[20px] justify-evenly shadow-xl rounded-[9px]`}>
+              <div className="bg-gray-300 rounded-[5px] w-[110%] h-[35px] flex items-center">
+                COLLECTION
+              </div>
+              <div className="font-bold">DESIGN BYME</div>
+              <div className="font-bold">MERCH</div>
+              <div className="font-bold">DROP</div>
+              <div className="font-bold">ARTWORK</div>
+              <div className="font-bold">ARTIST</div>
+            </div>
+          </div>
+          <div className="hidden xl:flex items-center gap-5">
+            <div className="bg-gray-500 w-[1px] h-[35px] "></div>
+            <div className="flex gap-3 text-[20px] font-bold">
+              <AiOutlineSearch
+                onClick={() => {
+                  console.log(searchClick);
+                  setSearchClick(!searchClick);
+                }}
+              />
+              <SlBasket />
+              <AiOutlineUser />
+            </div>
+          </div>
+        </div>
+      </Container>
+      {searchClick ? <Search boolean={searchClick} /> : <></>}
     </div>
   );
 }
