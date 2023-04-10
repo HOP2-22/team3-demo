f0602a6acc2ab8f35f22b3affce52e336e88334b
spaceishere <batocirdaramjav@gmail.com>
2023-04-10 16:49:41 +0800

    login user is done

--- frontend/src/pages/loginUser.jsx
+++ frontend/src/pages/loginUser.jsx
@@ -1,4 +1,47 @@
+import { useState } from "react";
+import Cookie from "js-cookie";
+import axios from "axios";
+
 export default function LoginUser() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkpass, setCheckPass] = useState("");
+
+  const EmailInput = async (event) => {
+    setEmail(event.target.value);
+  };
+  const PassInput = async (event) => {
+    setPassword(event.target.value);
+  };
+  const PassInputCheck = async (event) => {
+    setCheckPass(event.target.value);
+  };
+
+  const login = async () => {
+    if (password == checkpass) {
+      try {
+        console.log(email, password);
+        const res = await axios.post("http://localhost:7070/user/login", {
+          email: email,
+          password: password,
+        });
+        console.log("in process");
+        console.log("is done");
+        console.log(res.data);
+        Cookie.set("token", res.data?.token);
+        console.log(res?.data?.token);
+
+        alert("Амжилттай нэвтэрлээ");
+
+        // User_post();
+      } catch (error) {
+        alert("Нууц үг эсвэл Цахим хаяг буруу байна");
+      }
+    } else {
+      alert("Нууц үг таарахгүй байна");
+    }
+  };
+
   return (
     <div className="w-full h-full  mt-[150px] flex flex-col items-center justify-center gap-10">
       <div className="text-[32px] text-[#1b1927]">Нэвтрэх</div>
@@ -8,6 +51,7 @@ export default function LoginUser() {
           <input
             className="shadow-2xl w-[300px] sm:w-[380px] h-[45px] outline-none rounded-full text-[20px] px-[20px]"
             placeholder="••••••••••"
+            onChange={EmailInput}
           />
         </div>
         <div className="">
@@ -15,6 +59,7 @@ export default function LoginUser() {
           <input
             className="shadow-2xl w-[300px] sm:w-[380px] h-[45px] outline-none rounded-full text-[20px] px-[20px]"
             placeholder="••••••••••"
+            onChange={PassInput}
           />
         </div>
         <div className="">
@@ -22,9 +67,14 @@ export default function LoginUser() {
           <input
             className="shadow-2xl w-[300px] sm:w-[380px] h-[45px] outline-none rounded-full text-[20px] px-[20px]"
             placeholder="••••••••••"
+            onChange={PassInputCheck}
           />
         </div>
-        <button className="text-[20px] w-[280px] sm:w-[380px] h-[45px] rounded-full bg-[#1b1927] text-white">
+        <button
+          onClick={() => {
+            login();
+          }}
+          className="text-[20px] w-[280px] sm:w-[380px] h-[45px] rounded-full bg-[#1b1927] text-white">
           Нэвтрэх
         </button>
       </div>
