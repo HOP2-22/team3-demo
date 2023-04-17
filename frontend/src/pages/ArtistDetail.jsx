4fb907357c02af3be02cc0867d8d39e095bf55e7
E-Turtushig <Turtushig102@gmail.com>
2023-04-17 19:09:25 +0800

    test

--- frontend/src/pages/ArtistDetail.jsx
+++ frontend/src/pages/ArtistDetail.jsx
@@ -97,7 +97,7 @@ function ArtistDetail() {
                   color: "gray",
                 }}
               >
-                Улаанбаатар, Монгол
+                Улаанбаатар, Монголa
               </Box>
             </Box>
           </Box>
--- frontend/src/pages/artistProfile/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ArtistDetail from "@/components/ArtistDetail";
-export default function Artist() {
-  return (
-    <div className="artistMain">
-      <ArtistDetail />
-    </div>
-  );
-}
