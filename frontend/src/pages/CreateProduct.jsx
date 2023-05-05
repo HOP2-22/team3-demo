b6417f39051ee28357bce8c7d38f40a6cca1ecfb
22-hop2-2B-NandinErdne <22-hop2-nandinerdne@Mstars-Lab1-09s-iMac.local>
2023-05-05 19:39:50 +0800

    grhfdnb

--- frontend/src/pages/CreateProduct.jsx
+++ frontend/src/pages/CreateProduct.jsx
@@ -1,3 +1,38 @@
+import { Box, Button, Container } from "@mui/material";
+import axios from "axios";
+import { useState } from "react";
+
 export default function Create() {
-  return <div className="w-full h-[100vh] bg-slate-300"></div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const create = async () => {
+    try {
+      const res = axios.post("http://localhost:7070/product/create", {
+        email: email,
+        password: password,
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="w-full h-[100vh] bg-slate-300" style={{ padding: "40vh" }}>
+      <Box display="flex" justifyContent="space-around">
+        <input
+          onChange={(el) => {
+            setEmail(el.target.value);
+          }}
+        />
+        <input
+          onChange={(el) => {
+            setEmail(el.target.value);
+          }}
+        />
+      </Box>
+      <img src="cover-photo.gif" />
+      <Button onClick={() => create()}>click</Button>
+    </div>
+  );
 }
